ac37bb44bbaf2be9218302fd6848857e5121745c
Mervin Hemaraju <115166315+mervinhemaraju@users.noreply.github.com>
2024-11-13 20:07:48 +0400

    Fix/UI improvements (#16)
    * - Improved UI for smaller devices
    - Added typewriter effect for header
    
    * - Updated animations
    - Updated UI
    
    * - Code refinements

--- scripts/main.js
+++ scripts/main.js
@@ -1,45 +1,53 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
-  const tabs = document.querySelectorAll('#experienceTab li a');
-  const tabContents = document.querySelectorAll('#experienceTabContent > div');
+    // Get the tabs for the experience section
+    const tabs = document.querySelectorAll('#experienceTab li a');
+    const tabContents = document.querySelectorAll('#experienceTabContent > div');
 
-  document.getElementById('mobile-menu-button').addEventListener('click', function () {
-      var menu = document.getElementById('mobile-menu');
-      const iconOpen = document.getElementById('icon-open');
-      const iconClose = document.getElementById('icon-close');
-      menu.classList.toggle('hidden');
-      iconOpen.classList.toggle('hidden');
-      iconClose.classList.toggle('hidden');
-  });
+    // Adds animation to toggleButton experience
+    $("#toggleButton").click(function(){
+        $("#toggleDiv").slideToggle("slow");
+    });
 
-  tabs.forEach(tab => {
-      tab.addEventListener('click', function (event) {
-          event.preventDefault();
+    // Updates the menu icon
+    document.getElementById('mobile-menu-button').addEventListener('click', function () {
+        var menu = document.getElementById('mobile-menu');
+        const iconOpen = document.getElementById('icon-open');
+        const iconClose = document.getElementById('icon-close');
+        menu.classList.toggle('hidden');
+        iconOpen.classList.toggle('hidden');
+        iconClose.classList.toggle('hidden');
+    });
 
-          document.querySelectorAll('#experienceTab a').forEach(a => a.setAttribute(
-              'aria-selected', 'false'));
-          this.setAttribute('aria-selected', 'true');
-          document.querySelectorAll('#experienceTabContent > div').forEach(content =>
-              content.setAttribute('aria-hidden', 'true'));
-          document.querySelector(this.getAttribute('href')).setAttribute(
-              'aria-hidden', 'false');
+    // Experience tabs updates
+    tabs.forEach(tab => {
+        tab.addEventListener('click', function (event) {
+            event.preventDefault();
 
-          // Remove active class from all tabs
-          tabs.forEach(t => t.parentElement.classList.remove('active'));
-          tabs.forEach(t => t.classList.remove('active'));
+            document.querySelectorAll('#experienceTab a').forEach(a => a.setAttribute(
+                'aria-selected', 'false'));
+            this.setAttribute('aria-selected', 'true');
+            document.querySelectorAll('#experienceTabContent > div').forEach(content =>
+                content.setAttribute('aria-hidden', 'true'));
+            document.querySelector(this.getAttribute('href')).setAttribute(
+                'aria-hidden', 'false');
 
-          // Add active class to the clicked tab
-          this.parentElement.classList.add('active');
-          this.classList.add('active');
+            // Remove active class from all tabs
+            tabs.forEach(t => t.parentElement.classList.remove('active'));
+            tabs.forEach(t => t.classList.remove('active'));
 
-          // Hide all tab contents
-          tabContents.forEach(content => content.classList.add('hidden'));
+            // Add active class to the clicked tab
+            this.parentElement.classList.add('active');
+            this.classList.add('active');
 
-          // Show the clicked tab content
-          const target = document.querySelector(this.getAttribute('href'));
-          target.classList.remove('hidden');
-          target.classList.add('animate-slideUp');
-      });
-  });
+            // Hide all tab contents
+            tabContents.forEach(content => content.classList.add('hidden'));
+
+            // Show the clicked tab content
+            const target = document.querySelector(this.getAttribute('href'));
+            target.classList.remove('hidden');
+            target.classList.add('animate-slideUp');
+        });
+    });
 });
